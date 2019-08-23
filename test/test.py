c4750f61757a4be214d3c0024cc01f5439689cd3
rotsee <mejl@leowallentin.se>
2019-08-23 12:37:31 +0200

    test date

--- test/test.py
+++ test/test.py
@@ -6,3 +6,13 @@ def test_initing():
     se_7 = api["se-7"]
 
     assert(type(se_7).__name__ is "Dataset")
+
+def test_date():
+    api = Thenmap()
+    se_7 = api["se-7"]
+    assert(len(se_7.geojson["features"]) == 290)
+    se_7.date = "1975-05-03"
+    assert(len(se_7.geojson["features"]) == 277)
+    se_7.date = "1971-12-24"
+    assert(len(se_7.geojson["features"]) == 346)
+
